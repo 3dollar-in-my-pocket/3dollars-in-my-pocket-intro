0f679e34450995a873e39567c58ee0a3fdf2e0f8
from97 <skm1104@daum.net>
2022-01-11 21:55:31 +0900

    Creator 스타일드 컴포넌트 적용

--- src/pages/Creator.jsx
+++ src/pages/Creator.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 import { TitleStyled } from "../App";
 import CreatorList from "../components/CreatorList/CreatorList";
 import Footer from "../components/Footer/Footer";
 
 const Creator = () => {
   return (
-    <div className="page">
+    <CreatorStyled className="page">
       <TitleStyled>Creator</TitleStyled>
       <CreatorList />
       <Footer />
-    </div>
+    </CreatorStyled>
   );
 };
+
+const CreatorStyled = styled.div``;
 
 export default Creator;
